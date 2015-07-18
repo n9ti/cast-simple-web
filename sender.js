9eff7c7b547f29f054ac92aa8b09944f719444dd
Nati Namvong <nati@flyingcomma.com>
2015-07-18 19:52:32 +0700

    Update 2015-07-18T12:52:28.015Z

--- sender.js
+++ sender.js
@@ -2,8 +2,9 @@
 angular.module('myapp', ['ng-chrome-cast-channels'])
   .controller('myctrl', ['$scope', 'ChromeCastSenderChannel', function ($scope, ChromeCastSenderChannel) {
     // var cc = ChromeCastChannel
-    ChromeCastSenderChannel.initialize('7F0322BF', 'urn:x-cast:com.google.cast.sample')
+    ChromeCastSenderChannel.initialize('7F0322BF', 'urn:x-cast:com.google.cast.sample.helloworld')
     // Replace XXXXXXXX with your Application ID
+    $scope.message = 'Hello'
     $scope.click = function () {
       ChromeCastSenderChannel.sendMessage($scope.message)
     }
