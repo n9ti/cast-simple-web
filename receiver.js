33684b058c57c2342f225af2e788cbd7edff57b2
Nati Namvong <nati@flyingcomma.com>
2015-07-18 20:02:28 +0700

    Update 2015-07-18T13:02:23.731Z

--- receiver.js
+++ receiver.js
@@ -1,9 +1,11 @@
 /* global angular */
 angular.module('myapp', ['ng-chrome-cast-channels'])
-  .controller('myctrl', ['$scope', 'ChromeCastReceiverChannel', function ($scope, ChromeCastReceiverChannel) {
+  .controller('myctrl', ['$scope', '$sce', 'ChromeCastReceiverChannel', function ($scope, ChromeCastReceiverChannel, $sce) {
     $scope.callback = function (data) {
       $scope.data = data
+      $scope.frameUrl = $sce.trustAsResourceUrl(data)
       $scope.$apply()
     }
+
     ChromeCastReceiverChannel.initialize('urn:x-cast:com.google.cast.sample.helloworld', $scope.callback)
   }])
