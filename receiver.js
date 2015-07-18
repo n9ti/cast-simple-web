5c971d71d56f75489ec1dfd023063b157ea9a016
Nati Namvong <nati@flyingcomma.com>
2015-07-18 19:50:14 +0700

    Update 2015-07-18T12:50:09.411Z

--- receiver.js
+++ receiver.js
@@ -5,5 +5,5 @@ angular.module('myapp', ['ng-chrome-cast-channels'])
       $scope.data = data
       $scope.$apply()
     }
-    ChromeCastReceiverChannel.initialize('urn:x-cast:io.github.n9ti.cast.cast-simple-web', $scope.callback)
+    ChromeCastReceiverChannel.initialize('urn:x-cast:com.google.cast.sample.helloworld', $scope.callback)
   }])
--- sender.js
+++ sender.js
@@ -2,7 +2,7 @@
 angular.module('myapp', ['ng-chrome-cast-channels'])
   .controller('myctrl', ['$scope', 'ChromeCastSenderChannel', function ($scope, ChromeCastSenderChannel) {
     // var cc = ChromeCastChannel
-    ChromeCastSenderChannel.initialize('7F0322BF', 'urn:x-cast:io.github.n9ti.cast.cast-simple-web')
+    ChromeCastSenderChannel.initialize('7F0322BF', 'urn:x-cast:com.google.cast.sample.helloworld')
     // Replace XXXXXXXX with your Application ID
     $scope.click = function () {
       ChromeCastSenderChannel.sendMessage($scope.message)
