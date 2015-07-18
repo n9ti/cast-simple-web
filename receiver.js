97ff5fe40be49723664b3715c3edeb4a0005e051
Nati Namvong <nati@flyingcomma.com>
2015-07-18 20:17:28 +0700

    Update 2015-07-18T13:17:24.036Z

--- receiver.js
+++ receiver.js
@@ -2,8 +2,8 @@
 angular.module('myapp', ['ng-chrome-cast-channels'])
   .controller('myctrl', ['$scope', 'ChromeCastReceiverChannel', function ($scope, ChromeCastReceiverChannel) {
     $scope.callback = function (data) {
-      $scope.data = data
-      $scope.$apply()
+      // $scope.data = data
+      // $scope.$apply()
     }
 
     ChromeCastReceiverChannel.initialize('urn:x-cast:com.google.cast.sample.helloworld', $scope.callback)
